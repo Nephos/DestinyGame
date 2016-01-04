c7f4871512f2ddb8951d3e4eff3fcdad3a05eab9
Arthur Poulet <arthur.poulet@cryptolab.net>
2016-01-04 23:44:04 +0100

    add root_url

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "games#index"
   resources :games
   get 'games/:id/play', to: 'games#play', as: 'play_game'
   get 'games/:id/play/:dice', to: 'games#play_dice', as: 'play_dice_game'
